6430c3eb8559d95b7c3c89fb6c2ea5a7bb9aef95
iamsurajx <surajc8800@gmail.com>
2026-04-23 20:10:46 +0530

    footer social links added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const socials = [
  {
     name: 'GitHub',
-    href: 'https://github.com/surajchoudhary',
+    href: 'https://github.com/iamsurajx',
     icon: (
       <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
         <path fillRule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0 1 12 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0 0 22 12.017C22 6.484 17.522 2 12 2z" clipRule="evenodd"/>
@@ -21,7 +21,7 @@ const socials = [
   },
   {
     name: 'Twitter',
-    href: 'https://x.com/surajchoudhary',
+    href: 'https://x.com/suraj_code',
     icon: (
       <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
         <path d="M18.244 2H21.5l-7.1 8.11L22.5 22h-6.4l-5-6.54L5.4 22H2.1l7.6-8.7L1.5 2h6.5l4.5 5.95L18.24 2zm-1.1 18h1.8L7.1 4h-1.9l11.94 16z"/>
@@ -30,7 +30,7 @@ const socials = [
   },
   {
     name: 'Email',
-    href: 'mailto:surajchoudhary01@gmail.com',
+    href: 'mailto:surajofficemail+portfolio@gmail.com',
     icon: (
       <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
         <rect x="2" y="4" width="20" height="16" rx="2"/>
